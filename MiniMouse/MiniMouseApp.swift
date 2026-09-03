e7a27798fe80b5b21455eebda659e7972b801222
Mark Tomlin <Dygear@gmail.com>
2026-09-02 22:10:54 -0400

    Initial Commit

---
+++ MiniMouse/MiniMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMouseApp.swift
+//  MiniMouse
+//
+//  Created by Mark Tomlin on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
